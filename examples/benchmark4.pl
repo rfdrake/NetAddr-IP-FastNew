f51c8ee93def8076539ed412a42d1a2a50588744
Robert Drake <rdrake@direcpath.com>
2014-08-28 17:16:03 -0400

    splitting benchmarks
    Just makes it easier for me to run them depending on what I'm testing

--- examples/benchmark4.pl
+++ examples/benchmark4.pl
@@ -5,14 +5,9 @@ use lib './lib';
 use NetAddr::IP::FastNew;
 use NetAddr::IP;
 
-timethese(-3, {
+cmpthese(-3, {
   'NetAddr_IP' => sub { NetAddr::IP->new("127.0.0.1"); },
   'NetAddr_IP_FastNew_new_ipv4' => sub { NetAddr::IP::FastNew->new_ipv4("127.0.0.1"); },
   'NetAddr_IP_FastNew_new_ipv4_mask' => sub { NetAddr::IP::FastNew->new_ipv4_mask("127.0.0.0", '255.255.255.0'); },
   'NetAddr_IP_FastNew_new_ipv4_cidr' => sub { NetAddr::IP::FastNew->new_ipv4_cidr('127.0.0.0/24'); },
-});
-
-cmpthese(-3, {
-  'NetAddr_IP_ipv6' => sub { NetAddr::IP->new('fe80::/64'); },
-  'NetAddr_IP_FastNew_new_ipv6' => sub { NetAddr::IP::FastNew->new_ipv6('fe80::/64'); },
 });
